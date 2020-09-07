b43693ae2f40a2c43feb5cc1487acb37aebefafb
byungjun0520 <byungjun0520@gmail.com>
2020-09-07 14:38:48 +0900

    메소드 수정

--- src/main/java/com/global/biz/board/impl/BoardDAO.java
+++ src/main/java/com/global/biz/board/impl/BoardDAO.java
@@ -3,10 +3,16 @@ package com.global.biz.board.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
 
 import com.global.biz.board.BoardVO;
 import com.global.biz.comon.JDBCUtil;
 
+
+@Repository("boardDAO")
 public class BoardDAO {
 
 	// jdbc 관련변수
@@ -19,7 +25,7 @@ public class BoardDAO {
 	// 추가
 	private final String BOARD_INSERT =
 	"insert into board(seq, title, writer, content) "
-	+ "values((select nvl(max(seq),0)+1 from board), ?,?,?";
+	+ "values((select nvl(max(seq), 0)+1 from board), ?,?,?)";
 	
 	// 수정
 	private final String BOARD_UPDATE=
@@ -45,7 +51,7 @@ public class BoardDAO {
 			stmt.setString(1, vo.getTitle());
 			stmt.setString(2, vo.getWriter());
 			stmt.setString(3, vo.getContent());
-			stmt.executeLargeUpdate();
+			stmt.executeUpdate();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
@@ -81,7 +87,7 @@ public class BoardDAO {
 			conn = JDBCUtil.getConnection();
 			stmt = conn.prepareStatement(BOARD_DELETE);
 			stmt.setInt(1, vo.getSeq());
-			stmt.executeLargeUpdate();
+			stmt.executeUpdate();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
@@ -121,16 +127,39 @@ public class BoardDAO {
 	}
 	
 	
+	// 글 목록 조회
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public List<BoardVO> getBoardList(BoardVO vo) {
 		
+		System.out.println("===> JDBC로 getBoardList() 기능 처리 ..");
+		List<BoardVO> boardList = new ArrayList<BoardVO>();
+		
+		try {
+			
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(BOARD_LIST);
+			rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				BoardVO board = new BoardVO();
+				board.setSeq(rs.getInt("SEQ"));
+				board.setTitle(rs.getString("TITLE"));
+				board.setWriter(rs.getString("WRITER"));
+				board.setContent(rs.getString("CONTENT"));
+				board.setRegDate(rs.getDate("REGDATE"));
+				board.setCnt(rs.getInt("CNT"));
+				boardList.add(board);
+				
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		
+		return boardList;
+			
+	}
+	
 }
